56c1553c65d02683510da43f9ac6d4c3c5b97447
Ephraim Arizona <degreatarizona@gmail.com>
2024-10-29 11:18:20 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,115 @@
+function openEmoji(){
+  document.getElementById("emojis").style.height="250px";
+  document.getElementById("letters").style.height="0";
+  document.getElementById("theme").style.height="0";
+  document.getElementById("numbers").style.height="0";
+  document.getElementById("openl").style.display="block";
+  document.getElementById("opene").style.display="none";
+}
+function openletters(){
+  document.getElementById("letters").style.height="250px";
+  document.getElementById("theme").style.height="0";
+  document.getElementById("emojis").style.height="0";
+  document.getElementById("numbers").style.height="0";
+  document.getElementById("openl").style.display="none";
+  document.getElementById("opene").style.display="block";
+}
+function show(){
+  document.getElementById("letters").style.height="250px";
+  document.getElementById("theme").style.height="0";
+  document.getElementById("emojis").style.height="0";
+  document.getElementById("numbers").style.height="0";
+  document.getElementById("close").style.display="block";
+  document.getElementById("showk").style.display="none";
+}
+function closeB(){
+  document.getElementById("letters").style.height="0";
+  document.getElementById("theme").style.height="0";
+  document.getElementById("emojis").style.height="0";
+  document.getElementById("numbers").style.height="0";
+  document.getElementById("close").style.display="none";
+  document.getElementById("showk").style.display="block";
+}
+function magic() {
+  document.getElementById("letters").style.height="0";
+  document.getElementById("emojis").style.height="0";
+  document.getElementById("numbers").style.height="0";
+  document.getElementById("openl").style.display="block";
+  document.getElementById("opene").style.display="none";
+  document.getElementById("theme").style.height="250px";
+}
+function char() {
+  document.getElementById("letters").style.height="0";
+  document.getElementById("emojis").style.height="0";
+  document.getElementById("numbers").style.height="250px";
+  document.getElementById("openl").style.display="block";
+  document.getElementById("opene").style.display="none";
+  document.getElementById("theme").style.height="0";
+}
+function blue(){
+  document.getElementById('change').style.background="blue";
+  }
+function green(){
+  document.getElementById('change').style.background="green";
+  }
+function crimson(){
+  document.getElementById('change').style.background="crimson";
+  }
+function red(){
+  document.getElementById('change').style.background="red";
+  }
+function purple(){
+  document.getElementById('change').style.background="purple";
+  }
+function white(){
+  document.getElementById('change').style.background="white";
+  }
+function brown(){
+  document.getElementById('change').style.background="brown";
+  }
+function black(){
+  document.getElementById('change').style.background="black";
+  }
+function yellow(){
+  document.getElementById('change').style.background="yellow";
+  }
+function pink(){
+  document.getElementById('change').style.background="pink";
+  }
+function orange(){
+  document.getElementById('change').style.background="orange";
+  }
+function azure(){
+  document.getElementById('change').style.background="azure";
+  }
+  function blueviolet(){
+  document.getElementById('change').style.background="blueviolet";
+  }
+  function burlywood(){
+  document.getElementById('change').style.background="burlywood";
+  }
+  function gray(){
+  document.getElementById('change').style.background="gray";
+  }
+  function cornflowerblue(){
+  document.getElementById('change').style.background="cornflowerblue";
+  }
+  function darkblue(){
+  document.getElementById('change').style.background="darkblue";
+  }
+  function deeppink(){
+  document.getElementById('change').style.background="deeppink";
+  }
+  function firebrick(){
+  document.getElementById('change').style.background="firebrick";
+  }
+  function lightskyblue(){
+  document.getElementById('change').style.background="lightskyblue";
+  }
+  
+  function contact(){
+  
+  window.alert('Gmail: degreatarizona@gmail.com \n Yahoomail: degreatarizona53@yahoo.com \n Telegram Username:@DegreatArizona');
+  }
+  
+  
